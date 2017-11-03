8ea5a2be6c5ef013c84dd2bcc764e88212bbdb2e
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-11-03 11:16:41 +0000

    fifth test working

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,11 +1,15 @@
 def pet_shop_name(name)
-  return @pet_shop[:name]
+  return name[:name]
 end
 
 def total_cash(name)
-  return @pet_shop[:admin][:total_cash]
+  return name[:admin][:total_cash]
 end
 
 def add_or_remove_cash(shop, cash)
-  return @pet_shop[:admin][:total_cash] += cash
+  return shop[:admin][:total_cash] += cash
+end
+
+def pets_sold(name)
+  return name[:admin][:pets_sold]
 end
