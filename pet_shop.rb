05d94f741c2ce743ef91ea6c181864eaf6e35397
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-11-06 09:29:55 +0000

    HW checked with solutions

--- pet_shop.rb
+++ pet_shop.rb
@@ -77,10 +77,9 @@ def sell_pet_to_customer(pet_shop, pet, customer)
   if pet != nil && customer_can_afford_pet(customer, pet) == true
 
     add_or_remove_cash(pet_shop, pet[:price])
-
     add_pet_to_customer(customer, pet)
-
     increase_pets_sold(pet_shop, customer[:pets].length)
+    remove_pet_by_name(pet_shop, pet)
 
   else
     return customer_pet_count(customer)
